91e0d139f59f5d76284ae13f42a936d18f7ae3d8
Archish Venkatesh <Archish.Venkatesh@arm.com>
2025-05-07 10:59:36 +0530

    rdaspen: Platform support
    Add RD-Aspen platform support for trusted services.
    
    Change-Id: Ieea3ee1b0b6b95275064c6aa5db2f69c7dc06330
    Signed-off-by: Archish Venkatesh <Archish.Venkatesh@arm.com>
    Signed-off-by: Amr Mohamed <amr.mohamed@arm.com>
    Signed-off-by: David Hu <david.hu2@arm.com>
    Signed-off-by: Meet Patel <meet.patel2@arm.com>
    Signed-off-by: Musa Antike <musa.antike@arm.com>
    Signed-off-by: Ziad Elhanafy <ziad.elhanafy@arm.com>
    Signed-off-by: Gyorgy Szing <gyorgy.szing@arm.com>

---
+++ platform/providers/arm/automotive-rd/rdaspen/platform.cmake
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2025, Arm Limited and Contributors. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# Platform definition for the RD-Aspen platform.
+#-------------------------------------------------------------------------------
+
+set(SMM_GATEWAY_MAX_UEFI_VARIABLES 60 CACHE STRING "Maximum UEFI variable count")
+set(SMM_RPC_CALLER_SESSION_SHARED_MEMORY_SIZE 2*4096 CACHE STRING "RPC caller buffer size in SMMGW")
+set(SMM_SP_HEAP_SIZE 64*1024 CACHE STRING "SMM gateway SP heap size")
+
+target_compile_definitions(${TGT} PRIVATE
+	PLAT_RSE_COMMS_PAYLOAD_MAX_SIZE=0x2080
+	COMMS_MHU_MSG_SIZE=0xC
+)
+
+get_property(_platform_driver_dependencies TARGET ${TGT}
+	PROPERTY TS_PLATFORM_DRIVER_DEPENDENCIES
+)
+
+#-------------------------------------------------------------------------------
+#  Map platform dependencies to suitable drivers for this platform
+#
+#-------------------------------------------------------------------------------
+if ("mhu" IN_LIST _platform_driver_dependencies)
+	include(${TS_ROOT}/platform/drivers/arm/mhu_driver/mhu_v3_x/driver.cmake)
+endif()
