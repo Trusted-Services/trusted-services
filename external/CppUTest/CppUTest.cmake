65012dccb78611936ecc3d04a09a74bd800bcb3f
Gyorgy Szing <gyorgy.szing@arm.com>
2025-07-28 17:49:02 +0200

    cpputest: fix patch file related build error
    The external component was changed to use git am, but one of the patch
    files was not updated to the correct format. This made the build fail at
    the patching stage.
    
    Fix the patch file and make the build work again.
    
    Signed-off-by: Gyorgy Szing <gyorgy.szing@arm.com>
    Change-Id: I6615129eecce23d5d055b4146f6648d451814672

--- external/CppUTest/CppUTest.cmake
+++ external/CppUTest/CppUTest.cmake
@@ -19,7 +19,7 @@ set(GIT_OPTIONS
 	PATCH_COMMAND
 		git stash
 		COMMAND git branch -f bf-am
-		COMMAND git am ${CMAKE_CURRENT_LIST_DIR}/0001-cpputest-cmake-fix.patch
+		COMMAND git am ${CMAKE_CURRENT_LIST_DIR}/0001-Fix-cmake-problems.patch
 		COMMAND git am ${CMAKE_CURRENT_LIST_DIR}/0002-Fix-cmake-4.0-compatibility.patch
 		COMMAND git reset bf-am
 	)
