Trusted-Services/trusted-services

commit 3c0dc1fdbc7195c6ca9874a6e51ef145a99d357e

log_factory: refactor log_factory_create
List of current issues with log_factory_create():
- contains multiple return statements
- does not generate log entries when errors occur
- returns a pointer that is never used by any of its callers

Refactor the function to use a single return point to simplify and clean
up error handling, and add EMGS messages to improve visibility into the
system state during failures. Remove redundant log messages at the
function’s call sites.

Change-Id: I5aa35ddd1e4aa963b2ab41fbab63a018029149fb
Signed-off-by: Remy Thura <remy.thura@arm.com>

components/service/log/factory/log_factory.c
@@ -18,6 +18,7 @@
 #include "protocols/rpc/common/packed-c/status.h"
 #include "service/log/client/log_client.h"
 #include "sp_discovery.h"
+#include "trace.h"
 
 /**
  * A log factory that creates log backends that is used
@@ -38,35 +39,43 @@ static const struct rpc_uuid logging_service_uuid = { .uuid = TS_LOG_SERVICE_UUI
 /*
  * Log factory create that is included in the code for other SP's
  */
-struct log_backend *log_factory_create(void)
+void log_factory_create(void)
 {
 	struct logger *new_backend = &backend_instance;
-	rpc_status_t rpc_status = RPC_ERROR_INTERNAL;
+	rpc_status_t sp_init_status = RPC_ERROR_INTERNAL;
+	rpc_status_t session_open_status = RPC_ERROR_INTERNAL;
 	log_status_t log_status = LOG_STATUS_GENERIC_ERROR;
 
-	if (new_backend->in_use)
-		return NULL;
+	if (new_backend->in_use) {
+		log_status == LOG_STATUS_SUCCESS;
+		goto end;
+	}
 
-	rpc_status = ts_rpc_caller_sp_init(&new_backend->caller);
-	if (rpc_status != RPC_SUCCESS)
-		return NULL;
+	sp_init_status = ts_rpc_caller_sp_init(&new_backend->caller);
+	if (sp_init_status != RPC_SUCCESS){
+		DMSG("Failed to initialize SP: %d", sp_init_status);
+		goto end;
+	}
 
-	rpc_status = rpc_caller_session_find_and_open(&new_backend->session, &new_backend->caller,
+	session_open_status = rpc_caller_session_find_and_open(&new_backend->session, &new_backend->caller,
 						      &logging_service_uuid, 4096);
-	if (rpc_status != RPC_SUCCESS) {
-		(void)ts_rpc_caller_sp_deinit(&new_backend->caller);
-		return NULL;
+	if (session_open_status != RPC_SUCCESS) {
+		DMSG("Failed to open session: %d", session_open_status);
+		goto end;
 	}
 
 	log_status = log_client_init(&new_backend->client, &new_backend->session);
+	new_backend->in_use = (log_status == LOG_STATUS_SUCCESS);
+
+end:
 	if (log_status != LOG_STATUS_SUCCESS) {
-		(void)ts_rpc_caller_sp_deinit(&new_backend->caller);
-		return NULL;
+		if (sp_init_status == RPC_SUCCESS)
+			(void)ts_rpc_caller_sp_deinit(&new_backend->caller);
+
+		EMSG("Logging service discovery failed, falling back to console log: %d", log_status);
+	} else {
+		IMSG("Logging service discovery successful");
 	}
-
-	new_backend->in_use = true;
-
-	return log_factory_get_backend_instance();
 }
 
 /*

components/service/log/factory/log_factory.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2023, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2023-2026, Arm Limited and Contributors. All rights reserved.
  */
 
 #ifndef LOG_FACTORY_H
@@ -17,7 +17,7 @@ extern "C" {
 
 struct logger;
 
-struct log_backend *log_factory_create(void);
+void log_factory_create(void);
 struct log_backend *log_factory_get_backend_instance(void);
 void log_factory_destroy(struct logger *backend);
 

deployments/attestation/env/commonsp/attestation_sp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2021-2026, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -208,11 +208,7 @@ static bool sp_init(uint16_t *own_id)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	sp_res = sp_discovery_own_id_get(own_id);
 	if (sp_res != SP_RESULT_OK) {

deployments/block-storage/env/commonsp/block_storage_sp.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright (c) 2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2022-2026, Arm Limited and Contributors. All rights reserved.
  */
 
 #include "common/crc32/crc32.h"
@@ -140,11 +140,7 @@ static bool sp_init(uint16_t *own_id)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	sp_res = sp_discovery_own_id_get(own_id);
 	if (sp_res != SP_RESULT_OK) {

deployments/crypto/env/commonsp/crypto_sp.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2026, Arm Limited and Contributors. All rights reserved.
  */
 
 #include "components/rpc/common/endpoint/rpc_service_interface.h"
@@ -166,11 +166,7 @@ static bool sp_init(uint16_t *own_id)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	sp_res = sp_discovery_own_id_get(own_id);
 	if (sp_res != SP_RESULT_OK) {

deployments/ftpm/env/commonsp/ftpm_sp.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2024-2026, Arm Limited and Contributors. All rights reserved.
  */
 
 #include "common/trace/include/trace.h"
@@ -52,11 +52,7 @@ void __noreturn sp_main(union ffa_boot_info *boot_info)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	sp_res = sp_discovery_own_id_get(&own_id);
 	if (sp_res != SP_RESULT_OK) {

deployments/fwu/env/commonsp/fwu_sp.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * Copyright (c) 2023-2024, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2023-2026, Arm Limited and Contributors. All rights reserved.
  */
 
 #include <stddef.h>
@@ -178,11 +178,7 @@ static bool sp_init(uint16_t *own_id)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	sp_res = sp_discovery_own_id_get(own_id);
 	if (sp_res != SP_RESULT_OK) {

deployments/internal-trusted-storage/env/commonsp/its_sp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2026, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -42,11 +42,7 @@ void sp_main(union ffa_boot_info *boot_info)
 	}
 
 	IMSG("Start discovering logging service");
-	if (log_factory_create()) {
-		IMSG("Logging service discovery successful");
-	} else {
-		EMSG("Logging service discovery failed, falling back to console log");
-	}
+	log_factory_create();
 
 	result = sp_discovery_own_id_get(&own_id);
 	if (result != SP_RESULT_OK) {

commit bab7a18447a8493c13ca465305d3497bfaaf4d66

log_factory: fix typo
A typo was introduced in commit 3c0dc1fdbc7195c6ca9874a6e51ef145a99d357e
preventing the compilation (and proper functioning) of
log_factory_create.
This commit fixed this typo.

Change-Id: I9a3698d8136277c9587126b5e00b08e4e713eec5
Signed-off-by: Remy Thura <remy.thura@arm.com>

components/service/log/factory/log_factory.c
@@ -47,7 +47,7 @@ void log_factory_create(void)
 	log_status_t log_status = LOG_STATUS_GENERIC_ERROR;
 
 	if (new_backend->in_use) {
-		log_status == LOG_STATUS_SUCCESS;
+		log_status = LOG_STATUS_SUCCESS;
 		goto end;
 	}
 